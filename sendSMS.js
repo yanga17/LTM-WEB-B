bc94b9baba13575e8e25229851c70331c0bf909f
Yanga <yangagovuza@gmail.com>
2024-08-05 09:17:28 +0200

    changed the sms code

--- sendSMS.js
+++ sendSMS.js
@@ -2,15 +2,21 @@ require('dotenv').config({ path: './configuration.env' });
 //const twilio = require('twilio');
 //var ltmDbConn = require("../LTM-WEB-b/config/legendTimeDb.config");
 const ltmDbConn = require('./config/legendTimeDb.config');
+const twilio = require('twilio')
 
-const accountSID = process.env.TWILIO_ACC_SID
-const authToken = process.env.TWILIO_AUTH_TOKEN
+// const accountSID = process.env.TWILIO_ACC_SID
+// const authToken = process.env.TWILIO_AUTH_TOKEN
 
-if (!accountSID || !authToken) {
+// if (!accountSID || !authToken) {
+//     throw new Error('Twilio account SID and auth token are required');
+// }
+
+// const client = require('twilio')(accountSID, authToken);
+const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN)
+
+if (!client) {
     throw new Error('Twilio account SID and auth token are required');
 }
-
-const client = require('twilio')(accountSID, authToken);
 
 const formatPhoneNumber = (phoneNumber) => {
     if (phoneNumber.startsWith('0')) {
@@ -24,7 +30,7 @@ const sendSMS = async (clientName, phoneNumber) => {
         const newNumber = formatPhoneNumber(phoneNumber);
 
         let msgOptions = {
-            from: process.env.TWILIO_NUMBER,
+            from: '+13343360977',
             to: newNumber,
             body: `Dear ${clientName}, your ticket has been logged and is currently pending. Our support team will be working on it shortly. Thank you for your patience.`
         }
